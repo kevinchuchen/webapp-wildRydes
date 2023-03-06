207613c3bfdd715a6f7157baf1999f3ba4ff156c
Lim <kevinchuchen@gmail.com>
2023-03-06 17:22:55 +0300

    removes auth

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_i8p45QzHs', // e.g. eu-east-2_uXboG5pAb
-        userPoolClientId: 'f5cqan5tlq8g0gb96b0bs98', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'eu-west-1_PPkynf3wh', // e.g. eu-east-2_uXboG5pAb
+        userPoolClientId: '16kjssnfp8bm7v6ugksic72rl0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,

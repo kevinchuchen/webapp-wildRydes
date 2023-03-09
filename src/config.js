c8eba22ac71c87405f544a5b95ed8b4cc096b989
Lim <kevinchuchen@gmail.com>
2023-03-09 10:32:48 +0300

    links invoke URL with terraform-created API-GW

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: $API_GATEWAY_INVOKEURL // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }

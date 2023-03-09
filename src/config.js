74b70a3a9f42c7a18eeb26d6483c7e53247401ca
Lim <kevinchuchen@gmail.com>
2023-03-09 15:15:47 +0300

    invoke url test

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: "https://jk80ljjffh.execute-api.us-east-1.amazonaws.com/prod"//"https://1vzsf0mqze.execute-api.us-east-1.amazonaws.com/prod"//$API_GATEWAY_INVOKEURL // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: "https://1vzsf0mqze.execute-api.us-east-1.amazonaws.com/prod"//"https://jk80ljjffh.execute-api.us-east-1.amazonaws.com/prod"
     }
 }

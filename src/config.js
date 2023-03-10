825676a43459bf81bc11700927572be202ab4ee3
kevinchuchen <59314509+kevinchuchen@users.noreply.github.com>
2023-03-10 14:33:33 +0300

    test invoke url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: "https://0ujbpml6c3.execute-api.us-east-1.amazonaws.com/prod"//"https://jk80ljjffh.execute-api.us-east-1.amazonaws.com/prod"
+        invokeUrl: "https://2403wsiyp0.execute-api.us-east-1.amazonaws.com/prod"//"https://jk80ljjffh.execute-api.us-east-1.amazonaws.com/prod"
     }
 }
